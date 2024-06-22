9bc1381ec5e10554d66804263f9474bc0a284b91
Lucas Mathews <lucasmathews50@gmail.com>
2024-06-21 21:40:36 -0300

    Commit-SpringBoot

---
+++ meu-projeto/src/main/java/meu/projetoparaenviar/model/ClienteRepository.java
@@ -0,0 +1,10 @@
+package meu.projetoparaenviar.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente,Long>{
+
+}
